77f63ad78bdf46e8fe28e7543818879ec4f11ff0
Youngki <ygh3166@naver.com>
2021-04-08 00:21:23 +0900

    테스트코드 수정

--- src/main/java/com/example/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/example/book/springboot/web/dto/HelloResponseDto.java
@@ -2,7 +2,6 @@ package com.example.book.springboot.web.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 
 @Getter // 선언한 모든 필드의 get 메소드 생성
 @RequiredArgsConstructor // 선언한 모든 필드의 생성자 생성(final 붙은거만)
